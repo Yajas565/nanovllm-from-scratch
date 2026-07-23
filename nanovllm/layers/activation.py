ce00bbe6d4f3382957041717aa4cb47927a18a77
Yajas565 <shivhitayajusuman@gmail.com>
2026-07-23 13:10:07 +0530

    Implement Silu Activation

---
+++ nanovllm/layers/activation.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SiluAndMul(nn.Module):
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor :
+        x1, x2 = x.chunk(2, dim=-1)
+        return F.silu(x1) * x2
