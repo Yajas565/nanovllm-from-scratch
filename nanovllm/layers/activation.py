80ba1a3c31e4dadb38acf2e9a20793ab32442030
Yajas565 <shivhitayajusuman@gmail.com>
2026-07-23 13:14:18 +0530

    added torch.compile decorator

--- nanovllm/layers/activation.py
+++ nanovllm/layers/activation.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 
 class SiluAndMul(nn.Module):
 
+    @torch.compile
     def forward(self, x: torch.Tensor) -> torch.Tensor :
         x1, x2 = x.chunk(2, dim=-1)
         return F.silu(x1) * x2
